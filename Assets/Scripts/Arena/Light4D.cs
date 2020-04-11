617bbe43cde60f74aac2dafccc028c52f352377d
Narkok <nar-ste@yandex.ru>
2020-04-11 14:48:24 +0300

    Исправлен свет

---
+++ Assets/Scripts/Arena/Light4D.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Light4D: MonoBehaviour {
+
+    private void Awake() {
+        List<Vector3> points = new List<Vector3>() {
+            new Vector3(0.0f, -0.3f, 0.9f),
+            new Vector3(0.8f, -0.3f, -0.5f),
+            new Vector3(0.0f, 1.0f, 0.0f),
+            new Vector3(-0.8f, -0.3f, -0.5f)
+        };
+
+        foreach (Vector3 point in points) {
+            GameObject light = GOManager.Create("Arenas/DirectionalLight", transform);
+            light.transform.forward = point;
+        }
+    }
+}
