7357f9b38fb76bde08b0126bf3e4e7f2b8fe6f3c
Narkok <nar-ste@yandex.ru>
2020-04-24 01:17:44 +0300

    След полета ракет и пуль

--- Assets/Scripts/Weapon/Ammo.cs
+++ Assets/Scripts/Weapon/Ammo.cs
@@ -6,10 +6,20 @@ using UnityEngine;
 public class Ammo: MonoBehaviour {
 
     [SerializeField] public WeaponType weaponType;
+    private TrailRenderer _trail;
+    private float _trailInitialTime;
 
 
-    private void Start() {
+    private void Awake() {
         StartCoroutine(SelfDestruction());
+        _trail = GetComponentInChildren<TrailRenderer>();
+        _trailInitialTime = _trail.time;
+        _trail.time = 0;
+    }
+
+
+    void Update() {
+        _trail.time = Mathf.Min(_trail.time + 0.2f * Time.deltaTime, _trailInitialTime);
     }
 
 
