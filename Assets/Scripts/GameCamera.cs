f515ee78a8aea0652d91a70c2ca610489d255700
Narkok <nar-ste@yandex.ru>
2020-04-26 15:42:23 +0300

    Вертикальная фиксация камеры

--- Assets/Scripts/GameCamera.cs
+++ Assets/Scripts/GameCamera.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 public class GameCamera: MonoBehaviour {
 
     /// Настройки
-    // private bool keepVertical = false;
+    public bool keepVertical = false;
     private float mouseSensitivity = 2000;
     private int smoothTime = 15;
     private float smoothDelta = 0.02f;
-    private float forwardOffset = 3;
+    private float forwardOffset = 1;
     private float positionResetDelay = 1.7f;
     private float turnRotateSmooth = 0.15f;
 
@@ -46,46 +46,34 @@ public class GameCamera: MonoBehaviour {
         /// Плавное приближение к машине
         if (currentSmooth < smoothTime) { 
             currentSmooth += smoothDelta; 
-            if (currentSmooth < smoothTime / 5) {
-                _transform.LookAt(_lookAtTarget);
-                _transform.position = Vector3.Slerp(_transform.position, _positionTarget.position, currentSmooth * Time.fixedDeltaTime);
-                return;
-            }
         }
 
         /// Подворачивание камеры при повороте
+        float deltaSmooth = currentSmooth * Time.fixedDeltaTime;
         float turn = InputManager.Input(InputManager.Turn);
         float speedDelta = (_vehicle.Speed / _vehicle.maxSpeed);
         float camRot = turn * speedDelta * 60;
-        _cameraTarget.localRotation = Quaternion.Slerp(_cameraTarget.localRotation, Quaternion.Euler(0, 0, -camRot), turnRotateSmooth * currentSmooth * Time.fixedDeltaTime);
+        float localXRotation = _positionTarget.localRotation.eulerAngles.x;
+        _cameraTarget.localRotation = Quaternion.Slerp(_cameraTarget.localRotation, Quaternion.Euler(0, 0, -camRot), turnRotateSmooth * deltaSmooth);
+        _positionTarget.localRotation = Quaternion.Slerp(_positionTarget.localRotation, Quaternion.Euler(localXRotation, 0, camRot), turnRotateSmooth * deltaSmooth);
 
         /// Поворот камеры вокруг машины
-        float mouseX = InputManager.Input(InputManager.MouseX) / mouseSensitivity;
+        float mouseX = currentSmooth < smoothTime ? 0 : InputManager.Input(InputManager.MouseX) / mouseSensitivity;
         localZRotation = (localZRotation + mouseX) % 360;
         float rotate = (180 - Mathf.Abs(Mathf.Abs(localZRotation) - 180)) / 180;
-        
         float deltaZ = forwardOffset * (speedDelta * (1 - rotate));
-
         _lookAtTarget.Rotate(_lookAtTarget.up, mouseX, Space.World);
         _lookAtTarget.localPosition = initialLookAtTargetPosition + new Vector3(0, 0, deltaZ);
 
+        // Вычисление целевых позиций
         Quaternion currentRotation = _transform.rotation;
-        _transform.LookAt(_lookAtTarget);
-
-        /// Доделать приколюху с сохранением вертикали камеры
-        // if (keepVertical) {
-        //     float a = Vector3.Angle(_transform.up, _lookAtTarget.right);
-        //     float b = Vector3.Angle(_lookAtTarget.right, _transform.right);
-        //     float angle = b <= 90 ? (90 - a) : (90 + a);
-        //     Debug.Log(angle);
-        //     _transform.Rotate(_transform.forward, angle, Space.World);
-        // }
-
+        if (keepVertical) _transform.rotation = _positionTarget.rotation;
+        else _transform.LookAt(_lookAtTarget);
         Quaternion destinationRotation = _transform.rotation;
 
         /// Изменение положения камеры
-        _transform.rotation = Quaternion.Slerp(currentRotation, destinationRotation, 5 * currentSmooth * Time.fixedDeltaTime);
-        _transform.position = Vector3.Slerp(_transform.position, _positionTarget.position, currentSmooth * Time.fixedDeltaTime);
+        _transform.rotation = Quaternion.Slerp(currentRotation, destinationRotation, 5 * deltaSmooth);
+        _transform.position = Vector3.Slerp(_transform.position, _positionTarget.position, deltaSmooth);
 
         /// Сброс поворота камеры
         if (mouseX != 0) {
@@ -94,7 +82,7 @@ public class GameCamera: MonoBehaviour {
             positionResetInProgress = false;
         }
         if (positionResetInProgress) {
-            _lookAtTarget.localRotation = Quaternion.Slerp(_lookAtTarget.localRotation, Quaternion.Euler(0, 0, 0), turnRotateSmooth * currentSmooth * Time.fixedDeltaTime);
+            _lookAtTarget.localRotation = Quaternion.Slerp(_lookAtTarget.localRotation, Quaternion.Euler(0, 0, 0), turnRotateSmooth * deltaSmooth);
             positionResetInProgress = _lookAtTarget.localRotation != Quaternion.Euler(0, 0, 0);
         }
     }
