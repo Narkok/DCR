ea3e4af74b6d5674996a7d489042f89f8a5ab360
Narkok <nar-ste@yandex.ru>
2020-06-01 00:43:34 +0300

    Подготовка к асинхронным вычисления ИИ

--- Assets/Scripts/Vehicle/VehicleAIController.cs
+++ Assets/Scripts/Vehicle/VehicleAIController.cs
@@ -1,48 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class VehicleAIController: MonoBehaviour {
 
+    enum Purpose {
+        ReachPoint,
+        Inaction
+    }
+
     private WheelController _wheelController;
     private WeaponController _weaponController;
-    
 
-    void Awake() {
+    [SerializeField] private Purpose currentPurpose = Purpose.Inaction;
+
+    private Thread calculatorThread;
+    private bool isDisabled;
+
+
+    private void Awake() {
         _wheelController = GetComponent<WheelController>();
         _weaponController = GetComponent<WeaponController>();
-        StartCoroutine(Turn());
-        StartCoroutine(MachineGunShot());
-        StartCoroutine(SelectedWeaponShot());
+        
     }
 
 
-    private float steering = 1;
+    private void Start() {
+        startCalculating();
+    }
 
 
-    void Update() {
+    private void startCalculating() {
+        if (calculatorThread != null) calculatorThread.Abort();
+        calculatorThread = new Thread(calculator);
+        calculatorThread.Start();
+    }
+
+
+    private void calculator() {
+        while(true) {
+            calculatePurpose();
+            Thread.Sleep(2000);
+        }
+    }
+
+
+    /// Тут вычисляются цели, типа, до какой точки доехать, какую машику преследовать, обработка состояния и расчет нужд
+    private void calculatePurpose() {
+    }
+
+
+    /// Тут контролируется процесс выполнения вычисленной цели
+    private void Update() {
         _wheelController.Throttle = 1;
-        _wheelController.Steering = steering;
     }
 
 
-    private IEnumerator Turn() {
-        yield return new WaitForSeconds(Random.Range(1, 4));
-        steering = Random.Range(-1f, 1f);
-        StartCoroutine(Turn());
-    }
-
-
-    private IEnumerator MachineGunShot() {
-        yield return new WaitForSeconds(Random.Range(1, 4));
-        _weaponController.ShootFromMachineGun();
-        StartCoroutine(MachineGunShot());
-    }
-
-
-    private IEnumerator SelectedWeaponShot() {
-        yield return new WaitForSeconds(Random.Range(1, 4));
-        _weaponController.ShootFromSelectedWeapon();
-        StartCoroutine(SelectedWeaponShot());
+    /// На выходе убиваем поток калькулятора
+    private void OnDisable() {
+        if (calculatorThread != null) calculatorThread.Abort();
     }
 }
